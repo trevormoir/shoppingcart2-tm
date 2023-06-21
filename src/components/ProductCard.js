9dd4be2a4e2948048252ab87019f850789022ada
tm-clockworks <trevor.moir@yahoo.ca>
2023-06-21 15:14:23 -0400

    Update unused variables

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,44 +1,22 @@
 import { useCart } from '../context/CartContext';
 import "./ProductCard.css";
-import { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 
 export const ProductCard = ({product, cart, setCart, cartItem, setCartItem}) => {
-  const { addToCart, removeFromCart, cartList } = useCart();
+  const { addToCart } = useCart();
   const {id, name, price, image} = product;
-  const [isInCart, setIsInCart] = useState(false);
+  // const [isInCart, setIsInCart] = useState(false);
 
-  const handleAdd = (product) => {
-    if (cart.some(el => el.id === id)) {
-        const updatedCart = cart.map((todo) => (
-            todo.id === id ? {id: id, name: name, price: price, image: image, qty: todo.qty + 1} : todo
-        ));
-        setCart(updatedCart);
-    }
-    else {
-        const newCartItem = {
-            id: id,
-            name: name,
-            price: price,
-            image: image,
-            qty: 1
-        }
-        setCart([...cart, newCartItem]);
-    }
-
-    setCartItem({});
-  }
-
-  useEffect(() => {
-    const productIsInCart = cartList.find(cartItem => cartItem.id === product.id);
-    if (productIsInCart) {
-      setIsInCart(true);
-    }
-    else {
-      setIsInCart(false);
-    }
-  }, [cartList, product.id])
+  // useEffect(() => {
+  //   const productIsInCart = cartList.find(cartItem => cartItem.id === product.id);
+  //   if (productIsInCart) {
+  //     setIsInCart(true);
+  //   }
+  //   else {
+  //     setIsInCart(false);
+  //   }
+  // }, [cartList, product.id])
 
   return (
     <div className="productCard">
